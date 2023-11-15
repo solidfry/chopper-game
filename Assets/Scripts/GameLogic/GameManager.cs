c51e65ea1a1ec1acc900db44015d4f96c31a9e90
Elliot <ellioteweb@gmail.com>
2023-11-15 13:18:50 +1100

    added assets for level

--- Assets/Scripts/GameLogic/GameManager.cs
+++ Assets/Scripts/GameLogic/GameManager.cs
@@ -9,32 +9,92 @@ namespace GameLogic
 {
     public class GameManager : SingletonPersistent<GameManager>
     {
+        [Header("System Prefabs")]
         [SerializeField] NetworkManager networkManagerPrefab;
-        [SerializeField] CursorManager cursorManager;
-        [SerializeField] UIAudioManager uiAudioManager;
-        [SerializeField] ColourManager colourManager;
-        [SerializeField] NotificationManager notificationManager;
+        [SerializeField] CursorManager cursorManagerPrefab;
+        [SerializeField] UIAudioManager uiAudioManagerPrefab;
+        [SerializeField] ColourManager colourManagerPrefab;
+        [SerializeField] NotificationManager notificationManagerPrefab;
         
+        [field: Header("Active Systems")]
+        [field:SerializeField ] 
+        public NetworkManager NetworkManager { get; private set; }
+        private bool _networkManagerIsActive;
+        [field:SerializeField ] 
+        public CursorManager CursorManager { get; private set; }
+        private bool _cursorManagerIsActive;
+        [field:SerializeField ] 
+        public ColourManager ColourManager { get; private set; }
+        private bool _colourManagerIsActive;
+        [field:SerializeField ] 
+        public NotificationManager NotificationManager { get; private set; }
+        private bool _notificationManagerIsAction;
+        [field:SerializeField ] 
+        public UIAudioManager UIAudioManager { get; private set; }
+        private bool _uiAudioManagerIsActive;
+
+
+            
+        [SerializeField] [ReadOnly] 
+        private bool allSystemsActive;
+
+
+
+
+
         public override void Awake()
         {
             base.Awake();
 
-            if (NetworkManager.Singleton == null) 
-                Instantiate(networkManagerPrefab);
+            SetSystemsActive();
+        }
 
-            if(CursorManager.Instance == null)
-                Instantiate(cursorManager);
+        private void Start()
+        {
+            CheckAllSystemsActive();
+        }
+
+        void SetSystemsActive()
+        {
+            if (NetworkManager == null) 
+                NetworkManager = Instantiate(networkManagerPrefab);
             
-            if(UIAudioManager.Instance == null)
-                Instantiate(uiAudioManager);
+            if(CursorManager == null)
+                CursorManager = Instantiate(cursorManagerPrefab);
             
-            if(ColourManager.Instance == null)
-                Instantiate(colourManager);
+            if(UIAudioManager == null)
+                UIAudioManager = Instantiate(uiAudioManagerPrefab);
             
-            if(NotificationManager.Instance == null)
-                Instantiate(notificationManager);
+            if(ColourManager == null)
+                ColourManager = Instantiate(colourManagerPrefab);
+            
+            if(NotificationManager == null)
+                NotificationManager = Instantiate(notificationManagerPrefab);
         }
         
+        public bool CheckAllSystemsActive()
+        {
+            _networkManagerIsActive = NetworkManager != null;
+            _cursorManagerIsActive = CursorManager != null;
+            _uiAudioManagerIsActive = UIAudioManager != null;
+            _colourManagerIsActive = ColourManager != null;
+            _notificationManagerIsAction = NotificationManager != null;
+            allSystemsActive = _networkManagerIsActive && _cursorManagerIsActive && _uiAudioManagerIsActive && _colourManagerIsActive && _notificationManagerIsAction;
+            return allSystemsActive;
+        }
+        
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        
+        private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1) => CheckAllSystemsActive();
+
         public void QuitGame()
         {
             Application.Quit();
