24f1c98b23beb1a5a0cc6aeb3987e6b779583e36
Elliot <ellioteweb@gmail.com>
2023-11-27 20:48:21 +1100

    added mask to gamebounds

--- Assets/PlayerBoundsHandler.cs
+++ Assets/PlayerBoundsHandler.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 
 public class PlayerBoundsHandler : MonoBehaviour
 {
+    [SerializeField] LayerMask layerMask;
     private void OnTriggerExit(Collider other)
     {
-        GameEvents.OnPlayerOutOfBoundsEvent?.Invoke();
+        if(other.gameObject.layer == layerMask.value)
+            GameEvents.OnPlayerOutOfBoundsEvent?.Invoke();
 
         Debug.Log("Player went out of bounds");
     }
     
     private void OnTriggerEnter(Collider other)
     {
-        GameEvents.OnPlayerInBoundsEvent?.Invoke();
+        if(other.gameObject.layer == layerMask.value)
+            GameEvents.OnPlayerInBoundsEvent?.Invoke();
         
         Debug.Log("Player went in bounds");
     }
